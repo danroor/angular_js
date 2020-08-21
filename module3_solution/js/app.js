85c251b01940cd08fb4fe9e1b6fec8220c2cc55e
danroor <dan.roor@yandex.ru>
2020-08-21 17:00:17 +0300

    abcdef

--- module3_solution/js/app.js
+++ module3_solution/js/app.js
@@ -1,33 +1,81 @@
 (function () {
 'use strict';
 
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', controller);
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowCtrl)
+.service('MenuSearchService', MenuSrv)
+.directive('foundItems', FoundDir);
 
+function FoundDir () {
+	var ddo = {
+		scope: {
 
-function getMessage(items) {
-	if (items === undefined  || ! /\S/.test(items)) {
-		return "Please enter data first";
-	}
-	if (items.split(',').length <= 3) {
-		return "Enjoy!";
-	} 
-	
-	return "Too much!";
+			items : "<foundItems",
+			remove: "<onRemove"
+		},
+
+		restrict: 'E',
+		templateUrl: "found_items_template.html"
+
+	};
+
+	return ddo;
 }
 
+MenuSrv.$inject = ['$http'];
+function MenuSrv ($http) {
 
-controller.$inject = ['$scope'];
-function controller($scope) {
-	$scope.message = "";
-	$scope.displayMessage = function () {
-		$scope.message = getMessage($scope.items);	
-		if ($scope.message === "Please enter data first") {
-			$scope.color = 'red';
-		} else {
-			$scope.color = 'green';
+	this.getMatchedMenuItems = function (searchTerm) {
+		var onSuccess = function (response) {
+			var foundItems = [];
+			var allItems = response.data.menu_items;
+			var L = allItems.length;
+			
+			if (searchTerm === undefined  || ! /\S/.test(searchTerm)) {
+				return [];
+			}
+
+			searchTerm = searchTerm.toLowerCase();
+			
+			for (var i = 0; i < L; ++i) {
+				var desc = allItems[i].description.toLowerCase();
+
+				if (desc.indexOf(searchTerm) != -1) {
+					foundItems.push(allItems[i]);
+				}
+			}
+
+			return foundItems;
 		}
+
+		var request = {
+			method: "GET",
+			url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+		}
+
+		return $http(request).then(onSuccess);
 	}
+}
+
+NarrowCtrl.$inject = ['MenuSearchService'];
+function NarrowCtrl(MenuSearchService) {
+
+	var ctrl = this;
+
+	ctrl.found = [1];
+	ctrl.removeItem = function (index) {
+		ctrl.found.splice(index, 1);
+	}
+
+	ctrl.getMenuItems = function () {
+		var promise = MenuSearchService.getMatchedMenuItems(ctrl.term);
+		
+		promise.then( function (foundItems) {
+			ctrl.found = foundItems;
+		});
+	}
+
+
 }
 
 
